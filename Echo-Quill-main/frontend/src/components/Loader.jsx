ceda0cedd9b915fb1492ba172921f542cc84b323
swastik-mishra25 <128520442+swastik-mishra25@users.noreply.github.com>
2026-05-31 16:13:09 +0530

    upgradation

--- Echo-Quill-main/frontend/src/components/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Loader() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="loader"></div>
-    </div>
-  );
-}
